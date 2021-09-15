976c0bd63d09220fc78f682c4785ffc91304650f
lauraherreros <90603019+lauraherreros@users.noreply.github.com>
2021-09-15 10:52:57 +0200

    Update docker-run.sh

--- docker-run.sh
+++ docker-run.sh
@@ -7,18 +7,17 @@ git checkout lauraherreros_santander092021
 docker image build \
   --file ./Dockerfile \
   --no-cache \
-  --tag local\phpinfo:test \
+  --tag local/phpinfo:test \
   ./
- 
- 
+
 docker network create phpinfo
 docker container run \
---detach \
---name phpinfo \
---network phpinfo \
---read-only \
---restart always  \
---user nobody \
---volume ./src/index.php:/src/:ro \
---workdir /app/ \
-local/phpinfo:test \
+  --detach \
+  --name phpinfo \
+  --network phpinfo \
+  --read-only \
+  --restart always \
+  --user nobody \
+  --volume ./src/index.php:/app/index.php:ro \
+  --workdir /app/ \
+  local/phpinfo:test
